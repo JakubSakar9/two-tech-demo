a36138fd61fd4ee1499b6fb0f91917b6996226d3
Jakub Sakar <e12503814@student.tuwien.ac.at>
2026-02-17 13:17:14 +0100

    Improved terrain shading

--- entities/debug_texture/DebugTexture.cs
+++ entities/debug_texture/DebugTexture.cs
@@ -1,46 +1,27 @@
 using Godot;
 using System;
 
-public partial class DebugTexture : GridContainer
+public partial class DebugTexture : Control
 {
-    [Export] Terrain TerrainRef;
-    [Export] TerrainDeformer Deformer;
-    [Export] int CellTextureSize = 256;
-    
-    private TextureRect[] _texRects;
-
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
-        if (@event is InputEventKey keyEvent)
-        {
-            if (keyEvent.Keycode == Key.Q && keyEvent.IsPressed())
-            {
-                Visible = !Visible;
-            }
-        }
+        // if (@event is InputEventKey keyEvent)
+        // {
+        //     if (keyEvent.Keycode == Key.Q && keyEvent.IsPressed())
+        //     {
+        //         Visible = !Visible;
+        //     }
+        // }
     }
 
     public override void _Ready()
     {
         base._Ready();
-        _texRects = new TextureRect[9];
-        for (int i = 0; i < 9; i++)
-        {
-            _texRects[i] = new TextureRect();
-            _texRects[i].ExpandMode = TextureRect.ExpandModeEnum.IgnoreSize;
-            _texRects[i].CustomMinimumSize = new Vector2(CellTextureSize, CellTextureSize);
-            AddChild(_texRects[i]);
-        }
-        Deformer.CellChanged += QueueRedraw;
     }
 
     public override void _Draw()
     {
         base._Draw();
-        for (int i = 0; i < 9; i++)
-        {
-            _texRects[i].Texture = Deformer.DisplacementMaps[i];
-        }
     }
 }
--- entities/terrain/Terrain.cs
+++ entities/terrain/Terrain.cs
@@ -1,7 +1,56 @@
 using Godot;
-using System;
-using System.ComponentModel;
-using System.Threading.Tasks;
+
+public struct HeightMap
+{
+    public byte[] bytes;
+    public FastNoiseLite noiseFn;
+    public Image heightImage;
+    public ImageTexture height;
+    public NoiseTexture2D normal;
+    private int _size;
+
+    public HeightMap(int size)
+    {
+        bytes = new byte[size * size * sizeof(float)];
+        noiseFn = new();
+        heightImage = new();
+        height = new();
+        normal = new();
+        _size = size;
+
+        noiseFn.Offset = new Vector3(-size / 2.0f, -size / 2.0f, 0.0f);
+
+        normal.Noise = noiseFn;
+        normal.Width = _size;
+        normal.Height = _size;
+        normal.AsNormalMap = true;
+    }
+
+    public unsafe void Generate()
+    {
+        fixed(byte* bytePointer = bytes)
+        {
+            float* floatPointer = (float*)bytePointer;
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    float noiseValue = (noiseFn.GetNoise2D(j, i) + 1.0f) / 2.0f;
+                    floatPointer[i * _size + j] = noiseValue;
+                }
+            }
+        }
+        heightImage = Image.CreateFromData(_size, _size, false, Image.Format.Rf, bytes);
+        heightImage.GenerateMipmaps();
+        height.SetImage(heightImage);
+    }
+
+    public void MoveOrigin(Vector2 origin)
+    {
+        noiseFn.Offset = new Vector3(origin.X - _size/2, origin.Y - _size/2, 0.0f);
+        Generate();
+    }
+}
 
 public partial class Terrain : StaticBody3D
 {
@@ -21,9 +70,8 @@ public partial class Terrain : StaticBody3D
     private CollisionShape3D _terrainCollider;
     private HeightMapShape3D _heightMapShape;
 
-    private FastNoiseLite[] _noiseFunctions;
-    private ImageTexture[] _heightMaps;
-    private NoiseTexture2D[] _normalMaps;
+
+    private HeightMap[] _heightMaps;
 
     private FastNoiseLite _collisionNoiseFunction;
     private Image _collisionImage;
@@ -36,53 +84,55 @@ public partial class Terrain : StaticBody3D
         _terrainMesh = GetNode<MeshInstance3D>("%TerrainMesh");
         _terrainCollider = GetNode<CollisionShape3D>("%TerrainCollider");
         
-        _noiseFunctions = new FastNoiseLite[NOISE_SWAP_COUNT];
-        _heightMaps = new ImageTexture[NOISE_SWAP_COUNT];
-        _normalMaps = new NoiseTexture2D[NOISE_SWAP_COUNT];
+        _heightMaps = new HeightMap[NOISE_SWAP_COUNT];
         _heightMapShape = new HeightMapShape3D();
 
         int heightmapSize = 3 * ChunkSizeUnits;
 
         for (int i = 0; i < NOISE_SWAP_COUNT; i++)
         {
-            _noiseFunctions[i] = new FastNoiseLite();
-            _heightMaps[i] = new ImageTexture();
-            _normalMaps[i] = new NoiseTexture2D();
-
-            _noiseFunctions[i].FractalLacunarity = 1.7f;
-            _noiseFunctions[i].Offset = new Vector3(-1.5f * ChunkSizeUnits, -1.5f * ChunkSizeUnits, 0.0f);
-
-            byte[] imageData = new byte[heightmapSize * heightmapSize * sizeof(float)];
-            GenerateHeightMap(ref imageData, i);
-            var heightImage = Image.CreateFromData(3 * ChunkSizeUnits, 3 * ChunkSizeUnits, false, Image.Format.Rf, imageData);
-            heightImage.GenerateMipmaps();
-            GD.Print(heightImage.GetPixel(0, 0).R);
-            _heightMaps[i].SetImage(heightImage);
-            
-            _normalMaps[i].Noise = _noiseFunctions[i];
-            _normalMaps[i].Width = 3 * ChunkSizeUnits;
-            _normalMaps[i].Height = 3 * ChunkSizeUnits;
-            _normalMaps[i].AsNormalMap = true;
+            _heightMaps[i] = new(heightmapSize);
+            _heightMaps[i].noiseFn.FractalLacunarity = 1.7f;
+            _heightMaps[i].Generate();
         }
 
         _collisionNoiseFunction = new FastNoiseLite();
         UpdateCollisionHeightMap();
 
         (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("max_height", MaxHeight);
-        (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("height_map", _heightMaps[_noiseIndex]);
-        (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("normal_map", _normalMaps[_noiseIndex]);
+        (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("height_map", _heightMaps[_noiseIndex].height);
+        (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("normal_map", _heightMaps[_noiseIndex].normal);
         (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("snow_height", 0.3);
-
-        // GenDebugFloats();
-        // (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("tex_floats", _debugFloatsTex);
     }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
-        Vector2 position2D = new Vector2(Player.GlobalPosition.X, Player.GlobalPosition.Z);
-        _terrainMesh.GlobalPosition = new Vector3(position2D.X, 0, position2D.Y);
-        _CheckChunkChange(in position2D);
+        Vector2 playerPos2D = new Vector2(Player.GlobalPosition.X, Player.GlobalPosition.Z);
+        Vector2 pos2D = new Vector2(_terrainMesh.GlobalPosition.X, _terrainMesh.GlobalPosition.Z);
+        Vector2 positionDelta = playerPos2D - pos2D;
+        while (positionDelta.X > 4.0f / 3.0f)
+        {
+            positionDelta.X -= (4.0f / 3.0f);
+            pos2D.X += (4.0f / 3.0f);
+        }
+        while (positionDelta.X < -4.0f / 3.0f)
+        {
+            positionDelta.X += (4.0f / 3.0f);
+            pos2D.X -= (4.0f / 3.0f);
+        }
+        while (positionDelta.Y > 4.0f / 3.0f)
+        {
+            positionDelta.Y -= (4.0f / 3.0f);
+            pos2D.Y += (4.0f / 3.0f);
+        }
+        while (positionDelta.Y < -4.0f / 3.0f)
+        {
+            positionDelta.Y += (4.0f / 3.0f);
+            pos2D.Y -= (4.0f / 3.0f);
+        }
+        _terrainMesh.GlobalPosition = new Vector3(pos2D.X, 0, pos2D.Y);
+        _CheckChunkChange(in pos2D);
     }
 
     public void UpdateDisplacement(ref readonly Texture2D[] displacement)
@@ -92,12 +142,12 @@ public partial class Terrain : StaticBody3D
 
     public NoiseTexture2D GetNormalTexture()
     {
-        return _normalMaps[_noiseIndex];
+        return _heightMaps[_noiseIndex].normal;
     }
 
     public ImageTexture GetHeightMap()
     {
-        return _heightMaps[_noiseIndex];
+        return _heightMaps[_noiseIndex].height;
     }
 
     private void _CheckChunkChange(ref readonly Vector2 position2D)
@@ -149,7 +199,6 @@ public partial class Terrain : StaticBody3D
                 _collisionImage.SetPixel(j, i, new Color(value, 0.0f, 0.0f, 1.0f));
             }
         }
-        // NOTE: These numbers are arbitrarily set to make the collider work. It should be investigated why this is needed.
         _heightMapShape.UpdateMapDataFromImage(_collisionImage, 0f, MaxHeight);
         _terrainCollider.Shape = _heightMapShape;
         _terrainCollider.GlobalPosition = new Vector3(Player.GlobalPosition.X, 0, Player.GlobalPosition.Z);
@@ -158,35 +207,12 @@ public partial class Terrain : StaticBody3D
     private async void UpdateHeightMap()
     {
         _noiseIndex = (_noiseIndex + 1) % NOISE_SWAP_COUNT;
-        _noiseFunctions[_noiseIndex].Offset = new Vector3(ChunkOrigin.X - 1.5f * ChunkSizeUnits, ChunkOrigin.Y - 1.5f * ChunkSizeUnits, 0);
+        _heightMaps[_noiseIndex].MoveOrigin(ChunkOrigin);
         var timer = GetTree().CreateTimer(1.0f);
         await ToSignal(timer, SceneTreeTimer.SignalName.Timeout);
-        (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("height_map", _heightMaps[_noiseIndex]);
-        (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("normal_map", _normalMaps[_noiseIndex]);
+        (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("height_map", _heightMaps[_noiseIndex].height);
+        (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("normal_map", _heightMaps[_noiseIndex].normal);
         (_terrainMesh.MaterialOverride as ShaderMaterial).SetShaderParameter("chunk_origin", ChunkOrigin);
         EmitSignal(SignalName.MapShifted);
-    }
-
-    private unsafe void GenerateHeightMap(ref byte[] bytes, int noiseIndex = -1)
-    {
-        if (noiseIndex == -1)
-        {
-            noiseIndex = _noiseIndex;
-        }
-
-        int heightmapSize = 3 * ChunkSizeUnits;
-
-        fixed(byte* bytePointer = bytes)
-        {
-            float* floatPointer = (float*)bytePointer;
-            for (int i = 0; i < heightmapSize; i++)
-            {
-                for (int j = 0; j < heightmapSize; j++)
-                {
-                    float noiseValue = (_noiseFunctions[noiseIndex].GetNoise2D(j, i) + 1.0f) / 2.0f;
-                    floatPointer[i * heightmapSize + j] = noiseValue;
-                }
-            }
-        }
     }
 }
